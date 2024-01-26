c26300baa2b95fc39a152381ac2b272db3d38303
Nikolai Lønberg <niklonberg@gmail.com>
2024-01-26 11:29:01 +0100

    install ts, setup tsconfig

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@
 // app.use(cors());
 const express = require("express");
 const dotenv = require("dotenv");
+
+/*
+ * Load up and parse configuration details from
+ * the `.env` file to the `process.env`
+ * object of Node.js
+ */
 dotenv.config();
+
+/*
+ * Create an Express application and get the
+ * value of the PORT environment variable
+ * from the `process.env`
+ */
 const app = express();
 const port = process.env.PORT;
 
+/* Start the Express app and listen
+ for incoming requests on the specified port */
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
+/* Define a route for the root path ("/")
+ using the HTTP GET method */
 app.get("/", (req, res) => {
   res.send("hello world");
 });
