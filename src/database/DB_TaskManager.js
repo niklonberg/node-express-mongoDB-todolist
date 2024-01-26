396075069a1b878cf7012a4334425808c3415d6f
Nikolai Lønberg <niklonberg@gmail.com>
2024-01-26 14:16:22 +0100

    begin create DB_TaskManager, add manageDbOperation() & getTasks()

--- src/database/DB_TaskManager.js
+++ src/database/DB_TaskManager.js
@@ -2,6 +2,33 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 const MDBclient = new MongoClient(uri, { monitorCommands: true });
 
-class DB_TaskManager {}
+class DB_TaskManager {
+  constructor() {
+    this.tasks = MDBclient.db("TaskManager").collection("tasks");
+
+    // we could set up connection upon instantiation and keep it up?
+  }
+
+  // or use below function in all CRUD tasks?
+  async manageDbOperation(operation) {
+    try {
+      await MDBclient.connect();
+      console.log("Connection to TaskManager DB opened");
+      await operation();
+    } catch (error) {
+      console.error("Error setting up connection: ", error);
+    } finally {
+      await MDBclient.close();
+      console.log("Connection to TaskManager DB closed");
+    }
+  }
+
+  async getTasks() {
+    await this.manageDbOperation(async () => {
+      const tasks = await this.tasks.find().toArray();
+      return tasks;
+    });
+  }
+}
 
 export default DB_TaskManager;
